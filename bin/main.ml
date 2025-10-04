8b15ff34d60c697eee38ce23756a1a597fbbe3af
Yifei Sun <ysun@hey.com>
2025-10-04 14:12:20 +0200

    lib/config: config parsing (broken)

--- bin/main.ml
+++ bin/main.ml
@@ -1,40 +1,15 @@
-open Miroir.Config
+open Cmdliner
+open Miroir
 
-(* TODO *)
-type params =
-  { username : string (** Your Github username *)
-  ; api_key : string (** Your Github API key *)
-  ; command : string [@pos 0] [@docv "CMD"] (** The Github API command to run *)
-  ; dry_run : bool (** Don't really run this command *)
-  ; time_to_wait : float [@default 0.] (** Just an example of another type *)
-  }
-[@@deriving cmdliner, show]
+type params = { config : string [@aka [ "c" ]] [@default ""] [@env "MIROIR_CONFIG"] }
+[@@deriving cmdliner]
 
 let main () =
-  let test_config () =
-    let config_str = In_channel.with_open_text "config.toml" In_channel.input_all in
-    let config = parse_config config_str in
-    Printf.printf "Home: %s\n" config.general.home;
-    Printf.printf "Concurrency: %d\n" config.general.concurrency;
-    List.iter
-      (fun (name, p) ->
-         Printf.printf "Platform %s: %s@%s (origin=%b)\n" name p.user p.domain p.origin)
-      config.platform;
-    List.iter
-      (fun (name, r) ->
-         Printf.printf
-           "Repo %s: %s (visibility=%s, archived=%b)\n"
-           name
-           r.description
-           r.visibility
-           r.archived)
-      config.repo
-  in
-  let f p = show_params p |> print_endline in
-  let info = Cmdliner.Cmd.info Sys.argv.(0) in
-  let term = Cmdliner.Term.(const f $ params_cmdliner_term ()) in
-  let cmd = Cmdliner.Cmd.v info term in
-  exit (Cmdliner.Cmd.eval cmd)
+  let f p = Config.show_config p.config in
+  let info = Cmd.info "miroir" ~version:(Version.get ()) ~doc:"repo manager wannabe?" in
+  let term = Term.(const f $ params_cmdliner_term ()) in
+  let cmd = Cmd.v info term in
+  Cmd.eval cmd
 ;;
 
-let () = main ()
+let () = if !Sys.interactive then () else exit (main ())
--- lib/config.ml
+++ lib/config.ml
@@ -1,33 +1,55 @@
 open Ppx_deriving_toml_runtime
 
-(* https://github.com/andreypopp/ppx_deriving/blob/main/toml/ppx_deriving_toml_runtime.ml dont have ppx for map? *)
-
 type general =
   { home : string
+        [@toml.default "~/"]
+        (* the root directory of where users want to put all their repos at *)
   ; concurrency : int
-  ; env : (string * string) list
+        [@toml.default 1] (* number of parallelism if the task can be run concurrently *)
+  ; env : (string * string) list option [@toml.option]
+    (* environment variables to be made available *)
   }
+[@@deriving toml, show]
+
+type access =
+  | HTTPS
+  | SSH
 [@@deriving toml, show]
 
 type platform =
-  { origin : bool
-  ; domain : string
-  ; user : string
+  { origin : bool (* whether this git forge is considered as the fetch target *)
+  ; domain : string (* domain name for the git forge *)
+  ; user : string (* used to determine full repo url *)
+  ; access : access [@toml.default SSH] (* how to pull/push *)
   }
 [@@deriving toml, show]
 
+type visibility =
+  | Public
+  | Private
+[@@deriving toml, show]
+
 type repo =
-  { description : string
-  ; visibility : string
-  ; archived : bool
+  { description : string option [@toml.option] (* repo description *)
+  ; visibility : visibility [@toml.default Private] (* public or private *)
+  ; archived : bool [@toml.default false]
+    (* if true, repo will not be pulled/pushed, but metadata will still be managed *)
   }
 [@@deriving toml, show]
 
 type config =
   { general : general
-  ; platform : (string * platform) list
-  ; repo : (string * repo) list
+  ; platform : (string * platform) list [@toml.default []]
+  ; repo : (string * repo) list [@toml.default []]
   }
 [@@deriving toml, show]
 
-let parse_config toml_string = config_of_toml (Otoml.Parser.from_string toml_string)
+let parse_config str = config_of_toml (Otoml.Parser.from_string str)
+
+let show_config config_file =
+  let config =
+    In_channel.with_open_text config_file In_channel.input_all |> parse_config
+  in
+  Printf.printf "Home: %s\n" config.general.home;
+  Printf.printf "Concurrency: %d\n" config.general.concurrency
+;;
