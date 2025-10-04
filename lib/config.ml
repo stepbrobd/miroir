4863aebea298488c52ab76560833d0687d3ca1c4
Yifei Sun <ysun@hey.com>
2025-10-04 16:56:57 +0200

    lib/config: deriving toml cant handle assoc list

--- lib/config.ml
+++ lib/config.ml
@@ -1,3 +1,4 @@
+open Otoml
 open Ppx_deriving_toml_runtime
 
 type general =
@@ -6,15 +7,30 @@ type general =
         (* the root directory of where users want to put all their repos at *)
   ; concurrency : int
         [@toml.default 1] (* number of parallelism if the task can be run concurrently *)
-  ; env : (string * string) list option [@toml.option]
+  ; env : (string * string) list [@toml.default []]
     (* environment variables to be made available *)
   }
 [@@deriving toml, show]
 
+(* weird shit, cant use deriving toml *)
 type access =
   | HTTPS
   | SSH
-[@@deriving toml, show]
+[@@deriving show]
+
+let access_to_toml = function
+  | HTTPS -> TomlString "https"
+  | SSH -> TomlString "ssh"
+;;
+
+let access_of_toml = function
+  | TomlString s ->
+    (match String.lowercase_ascii s with
+     | "https" -> HTTPS
+     | "ssh" -> SSH
+     | _ -> of_toml_error "expected either `https` or `ssh`")
+  | _ -> of_toml_error "expected string value for access"
+;;
 
 type platform =
   { origin : bool (* whether this git forge is considered as the fetch target *)
@@ -24,10 +40,25 @@ type platform =
   }
 [@@deriving toml, show]
 
+(* another weird shit *)
 type visibility =
   | Public
   | Private
-[@@deriving toml, show]
+[@@deriving show]
+
+let visibility_to_toml = function
+  | Public -> TomlString "public"
+  | Private -> TomlString "private"
+;;
+
+let visibility_of_toml = function
+  | TomlString s ->
+    (match String.lowercase_ascii s with
+     | "public" -> Public
+     | "private" -> Private
+     | _ -> of_toml_error "expected either `public` or `private`")
+  | _ -> of_toml_error "expected string value for visibility"
+;;
 
 type repo =
   { description : string option [@toml.option] (* repo description *)
@@ -44,12 +75,54 @@ type config =
   }
 [@@deriving toml, show]
 
-let parse_config str = config_of_toml (Otoml.Parser.from_string str)
+let config_of_toml toml =
+  let table_to_assoc_list of_toml = function
+    | TomlTable items | TomlInlineTable items ->
+      List.map (fun (k, v) -> k, of_toml v) items
+    | _ -> of_toml_error "expected a table"
+  in
+  let get_table_opt key items =
+    try Some (List.assoc key items) with
+    | Not_found -> None
+  in
+  (* this ugly hunk of junk is needed because *i think* deriving toml cant natively parse ('a * 'b) list into tables *)
+  match toml with
+  | TomlTable items ->
+    let general =
+      match get_table_opt "general" items with
+      | Some (TomlTable gen_items) ->
+        let env =
+          match get_table_opt "env" gen_items with
+          | Some (TomlTable env_items | TomlInlineTable env_items) ->
+            List.map (fun (k, v) -> k, string_of_toml v) env_items
+          | Some _ -> of_toml_error "expected env to be a table"
+          | None -> []
+        in
+        let gen_without_env = List.remove_assoc "env" gen_items in
+        let g = general_of_toml (TomlTable gen_without_env) in
+        { g with env }
+      | Some _ -> of_toml_error "expected general to be a table"
+      | None -> of_toml_error "missing required field: general"
+    in
+    let platform =
+      match get_table_opt "platform" items with
+      | Some t -> table_to_assoc_list platform_of_toml t
+      | None -> []
+    in
+    let repo =
+      match get_table_opt "repo" items with
+      | Some t -> table_to_assoc_list repo_of_toml t
+      | None -> []
+    in
+    { general; platform; repo }
+  | _ -> of_toml_error "expected root to be a table"
+;;
+
+let config_of_string str = config_of_toml (Parser.from_string str)
 
 let show_config config_file =
-  let config =
-    In_channel.with_open_text config_file In_channel.input_all |> parse_config
-  in
-  Printf.printf "Home: %s\n" config.general.home;
-  Printf.printf "Concurrency: %d\n" config.general.concurrency
+  In_channel.with_open_text config_file In_channel.input_all
+  |> config_of_string
+  |> show_config
+  |> print_endline
 ;;
