bfbecd881d90dec84aa002a8f43ccdda26f2860d
Yifei Sun <ysun@hey.com>
2025-10-05 16:17:00 +0200

    lib/config: reorder interface entries

--- src/lib/config.mli
+++ src/lib/config.mli
@@ -4,17 +4,17 @@ type general =
   ; env : (string * string) list
   }
 
-val general_of_toml : Otoml.t -> general
-val general_to_toml : general -> Otoml.t
 val show_general : general -> Ppx_deriving_runtime.string
+val general_to_toml : general -> Otoml.t
+val general_of_toml : Otoml.t -> general
 
 type access =
   | HTTPS
   | SSH
 
+val show_access : access -> Ppx_deriving_runtime.string
 val access_to_toml : access -> Otoml.t
 val access_of_toml : Otoml.t -> access
-val show_access : access -> Ppx_deriving_runtime.string
 
 type platform =
   { origin : bool
@@ -23,17 +23,17 @@ type platform =
   ; access : access
   }
 
+val show_platform : platform -> Ppx_deriving_runtime.string
 val platform_of_toml : Otoml.t -> platform
 val platform_to_toml : platform -> Otoml.t
-val show_platform : platform -> Ppx_deriving_runtime.string
 
 type visibility =
   | Public
   | Private
 
+val show_visibility : visibility -> Ppx_deriving_runtime.string
 val visibility_to_toml : visibility -> Otoml.t
 val visibility_of_toml : Otoml.t -> visibility
-val show_visibility : visibility -> Ppx_deriving_runtime.string
 
 type repo =
   { description : string option
@@ -41,9 +41,9 @@ type repo =
   ; archived : bool
   }
 
+val show_repo : repo -> Ppx_deriving_runtime.string
 val repo_of_toml : Otoml.t -> repo
 val repo_to_toml : repo -> Otoml.t
-val show_repo : repo -> Ppx_deriving_runtime.string
 
 type config =
   { general : general
@@ -51,7 +51,7 @@ type config =
   ; repo : (string * repo) list
   }
 
+val show_config : config -> Ppx_deriving_runtime.string
 val config_to_toml : config -> Otoml.t
 val config_of_toml : Otoml.t -> config
 val config_of_string : string -> config
-val show_config : config -> Ppx_deriving_runtime.string
