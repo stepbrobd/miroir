afb4d283a5fa64d35117d04b55fddeb1599cc093
Yifei Sun <ysun@hey.com>
2026-03-04 19:19:46 +0100

    ocaml: remove lib/forge
    replaced by internal/forge in go rewrite

--- src/lib/forge/forge.ml
+++
@@ -1,9 +0,0 @@
-include Forge_intf
-
-let dispatch (forge : Miroir.Config.forge) : (module S) =
-  match forge with
-  | Github -> (module Github)
-  | Gitlab -> (module Gitlab)
-  | Codeberg -> (module Codeberg)
-  | Sourcehut -> (module Sourcehut)
-;;
