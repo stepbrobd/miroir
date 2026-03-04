54aaef78ef0d2eeb5b103ff347fb5f3d5fa15ef3
Yifei Sun <ysun@hey.com>
2026-03-04 19:19:52 +0100

    ocaml: remove lib (config, context, display, fetch, version)
    replaced by internal/{config,context,display,forge} in go rewrite

--- src/lib/version.ml
+++
@@ -1,5 +0,0 @@
-let get () =
-  match Build_info.V1.version () with
-  | Some v -> Build_info.V1.Version.to_string v
-  | None -> "git"
-;;
--- src/lib/version.mli
+++
@@ -1,2 +0,0 @@
-(** build version from dune-build-info, falls back to "git" *)
-val get : unit -> string
