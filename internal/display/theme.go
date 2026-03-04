b3205a3f00a1f026aa3e7d4a2d42b5a6d8fb7f65
Yifei Sun <ysun@hey.com>
2026-03-04 22:06:08 +0100

    internal/display: remove unused Error theme field
    Error style was defined in all themes but never referenced by the
    display code or any caller.

--- internal/display/theme.go
+++ internal/display/theme.go
@@ -6,35 +6,30 @@ type Theme struct {
 	Repo   lipgloss.Style
 	Remote lipgloss.Style
 	Output lipgloss.Style
-	Error  lipgloss.Style
 }
 
 var Nord = Theme{
 	Repo:   lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("#88C0D0")),
 	Remote: lipgloss.NewStyle().Foreground(lipgloss.Color("#EBCB8B")).PaddingLeft(2),
 	Output: lipgloss.NewStyle().Foreground(lipgloss.Color("#4C566A")).PaddingLeft(4),
-	Error:  lipgloss.NewStyle().Foreground(lipgloss.Color("#BF616A")),
 }
 
 var Catppuccin = Theme{
 	Repo:   lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("#89DCEB")),
 	Remote: lipgloss.NewStyle().Foreground(lipgloss.Color("#F9E2AF")).PaddingLeft(2),
 	Output: lipgloss.NewStyle().Foreground(lipgloss.Color("#585B70")).PaddingLeft(4),
-	Error:  lipgloss.NewStyle().Foreground(lipgloss.Color("#F38BA8")),
 }
 
 var Gruvbox = Theme{
 	Repo:   lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("#83A598")),
 	Remote: lipgloss.NewStyle().Foreground(lipgloss.Color("#FABD2F")).PaddingLeft(2),
 	Output: lipgloss.NewStyle().Foreground(lipgloss.Color("#665C54")).PaddingLeft(4),
-	Error:  lipgloss.NewStyle().Foreground(lipgloss.Color("#FB4934")),
 }
 
 var Plain = Theme{
 	Repo:   lipgloss.NewStyle(),
 	Remote: lipgloss.NewStyle().PaddingLeft(2),
 	Output: lipgloss.NewStyle().PaddingLeft(4),
-	Error:  lipgloss.NewStyle(),
 }
 
 var Themes = map[string]Theme{
